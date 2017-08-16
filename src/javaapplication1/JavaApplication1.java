494d4c887c784d7557823a719d154c12287e87b6
Max <Max@LAPTOP-S93TPE4B.fritz.box>
2017-08-16 16:31:20 +0200

    new commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,12 @@
+
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+    //MaxOrtner    
+     
+    public static void main(String[] args) {
+    System.out.println("HelloWorld");    }
+    
+}
